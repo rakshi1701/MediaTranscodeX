c054882c7ccde54a089420ec60176a6db0c706a9
rakshith <rakshithr1701@gmail.com>
2026-08-06 06:49:20 +0530

    Architecture of project

--- buid/CMakeFiles/Media_TranscodeX.dir/Media_TranscodeX_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Media_TranscodeX.dir/Media_TranscodeX_autogen/mocs_compilation.cpp.o: \
- /home/rakshith/my_works/Media_TranscodeX/buid/Media_TranscodeX_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- buid/CMakeFiles/Media_TranscodeX.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Media_TranscodeX.
-# This may be replaced when dependencies are built.
